b98e571f574bd4a0acb53d71e0fdfb1e3ef13724
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-11 22:47:22 +0300

    Initial commit

---
+++ Redecorating/Program.cs
@@ -0,0 +1,23 @@
+﻿class Redecorating
+{
+    public static void Main(string[] args)
+    {
+        int nylon = int.Parse(Console.ReadLine());
+        int paint = int.Parse(Console.ReadLine());
+        int thinner = int.Parse(Console.ReadLine());
+        int hours = int.Parse(Console.ReadLine());
+
+        double nylonPrice = 1.50;
+        double paintPrice = 14.50;
+        double thinnerPrice = 5.00;
+        
+
+
+        double materialsPrice =(nylon+2)*nylonPrice+paint*1.1*paintPrice+thinnerPrice*thinner+0.40;
+        double craftmenPrice=materialsPrice*hours*0.3;
+        double totalAmount=materialsPrice+craftmenPrice;
+        
+        Console.WriteLine(totalAmount);
+        
+    }
+}
